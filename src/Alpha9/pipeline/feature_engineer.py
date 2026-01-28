0916bf634fb240c2ad803c75dcb9c71f6e39a52b
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2026-01-28 23:07:13 +0530

    Progress on Feature Engineering - exploration

--- src/Alpha9/pipeline/feature_engineer.py
+++ src/Alpha9/pipeline/feature_engineer.py
@@ -1,2 +1,31 @@
-def engineer():
-    pass
+import talib as ta
+
+def engineer(data,
+             symbols,
+             timeperiod_cat1,
+             data_dir):
+    for symbol in symbols:
+        symbol = symbol.split('/')[0]
+        featured_data = data[symbol]
+
+        open_px = featured_data['open']
+        high_px = featured_data['high']
+        low_px = featured_data['low']
+        close_px = featured_data['close']
+        vol = featured_data['volume']
+
+        # trend indicators
+        featured_data['adx'] = ta.ADX(high_px, low_px, close_px, timeperiod=timeperiod_cat1)
+
+        # momentum indicators
+
+
+        # volatility indicators
+
+
+        # volume indicators
+
+
+        featured_data.dropna(inplace=True)
+        featured_data.to_csv(data_dir / f'{symbol}.csv', index=True)
+    return "Success"
--- src/Alpha9/pipeline/main_pipeline.py
+++ src/Alpha9/pipeline/main_pipeline.py
@@ -1,19 +1,26 @@
-from src.Alpha9.utility import get_config, get_path
+from src.Alpha9.pipeline import feature_engineer
+from src.Alpha9.utility import get_config, get_path, read_file
 
 config = get_config.load()
 
 def run():
     # load and handle paths
     raw_data_dir = get_path.absolute(config['path']['data']['raw'])
+    featured_data_dir = get_path.absolute(config['path']['data']['featured'])
     get_path.check(raw_data_dir)
+    get_path.check(featured_data_dir)
 
-    # load and handle other constants
+    # load and handle data downloading constants
     exchange = config['pipeline']['exchange']
     symbols = config['pipeline']['symbols']
     timeframe = config['pipeline']['timeframe']
     start_date = config['pipeline']['start_date']
     end_date = config['pipeline']['end_date']
 
+    # load and handle feature engineering constants
+    timeperiod_cat1 = config['pipeline']['timeperiod_cat1']
+
+    # handle data downloading
     choice = input('Download the data [y/n] : ')
     if choice.lower() == 'y':
         from src.Alpha9.pipeline import download_data
@@ -23,3 +30,22 @@ def run():
             print('Successfully downloaded the data!')
         else:
             print(f'Failed to download the data : {status}')
+            return status
+    else:
+        print("Using pre-downloaded data...")
+
+    # handle feature engineering
+    print("Creating features...")
+    data = {}
+    for symbol in symbols:
+        symbol = symbol.split('/')[0]
+        df = read_file.read_data('raw', symbol)
+        data[symbol] = df
+    status = feature_engineer.engineer(data, symbols, timeperiod_cat1, featured_data_dir)
+    if status == 'Success':
+        print('Successfully engineered the features!')
+    else:
+        print(f'Failed to engineer the features : {status}')
+        return status
+
+    return "Success"
