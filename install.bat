89e71fcad95ff7fd33b13de554b51bac7b455d8f
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2026-01-30 12:27:55 +0530

    Fix bugs

---
+++ install.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Installing Alpha9...
+pip install -e . --extra-index-url https://download.pytorch.org/whl/cu126
+pause
--- src/Alpha9/utility/get_config.py
+++ src/Alpha9/utility/get_config.py
@@ -1,6 +1,6 @@
 import yaml
 
-from src.Alpha9.utility import get_path
+from Alpha9.utility import get_path
 
 config_path = get_path.absolute('config.yaml')
 with open(config_path, 'r') as f:
