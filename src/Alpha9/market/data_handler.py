9bc0a4c3ef126b98a85a5d1135dbe0e6af250acc
DivyanshGupta2006 <thedivyanshgupta.100@gmail.com>
2026-03-22 18:33:41 +0530

    Defined market Schemas

---
+++ src/Alpha9/market/data_handler.py
@@ -0,0 +1,18 @@
+class DataHandler:
+    def get_latest_candle(self):
+        pass
+
+    def get_latest_candles(self, N=1):
+        pass
+
+    def get_latest_candle_datetime(self):
+        pass
+
+    def get_latest_candle_value(self, val):
+        pass
+
+    def get_latest_candles_value(self, val, N=1):
+        pass
+
+    def update_candles(self):
+        pass
